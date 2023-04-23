878ceafa02b40b6dc80164473a02f2b77ff3e04b
oxey:yum <lucoerlemans37@gmail.com>
2023-04-24 01:07:31 +0200

    make default layout char-based

--- gen-core/src/layout.rs
+++ gen-core/src/layout.rs
@@ -1,25 +1,21 @@
-use arrayvec::ArrayVec;
-
-pub type Key = usize;
-
 pub const ROW_AMOUNT: usize = 3;
 pub const COL_AMOUNT: usize = 10;
 pub const KEY_AMOUNT: usize = ROW_AMOUNT * COL_AMOUNT;
 
-pub type Matrix = ArrayVec<Key, KEY_AMOUNT>;
+pub type Matrix = [char; KEY_AMOUNT];
 
 pub struct Layout {
     matrix: Matrix,
 }
 
 impl Layout {
-    pub fn new(keys: [Key; KEY_AMOUNT]) -> Self {
+    pub fn new(keys: [char; KEY_AMOUNT]) -> Self {
         Self {
-            matrix: keys.into(),
+            matrix: keys,
         }
     }
 
-    pub fn keys(&self) -> &[Key] {
+    pub fn keys(&self) -> &[char] {
         &self.matrix
     }
 }
